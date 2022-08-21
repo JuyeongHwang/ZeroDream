b21b672f03bb29bd4afe60c1c8d8dd12d944d8c1
황주영 <49023736+JuyeongHwang@users.noreply.github.com>
2022-08-22 03:43:22 +0900

    대관람차 추가

--- ReZeroDream/Assets/Scripts/objectRotation.cs
+++ ReZeroDream/Assets/Scripts/objectRotation.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class objectRotation : MonoBehaviour
 {
+
+    public bool ferris = false;
+    public GameObject[] ferrisObjs;
+    
     [Range(0, 5)]
     public float speed = 2.0f;
+
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles -= new Vector3(0, speed, 0) * Time.deltaTime;
+        if (ferris)
+        {
+            for(int i = 0; i<ferrisObjs.Length; i++)
+            {
+                ferrisObjs[i].transform.position = transform.GetChild(i).transform.position;
+            }
+            transform.eulerAngles -= new Vector3(0, 0, speed) * Time.deltaTime;
+        }
+        else
+        {
+
+            transform.eulerAngles -= new Vector3(0, speed, 0) * Time.deltaTime;
+        }
     }
 }
